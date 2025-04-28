48998fe266e739b57f19ec9250e3f112e7d52bfc
Pranav S P <pranavsp93@gmail.com>
2025-04-28 11:48:31 +0530

    ✨ FEAT: Add JSON reporter to Playwright configuration for enhanced test result output

--- playwright.config.ts
+++ playwright.config.ts
@@ -32,6 +32,7 @@ export default defineConfig({
       },
     ],
     ["line"],
+    ["json", { outputFile: "results.json" }],
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
